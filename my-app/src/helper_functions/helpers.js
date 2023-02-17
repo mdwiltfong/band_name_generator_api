6408a6cf3bb671a3f78e32453ae06cf7cd44c5cf
mdwiltfong <wiltfong.michael1@gmail.com>
2023-02-17 12:51:12 -0500

    BandNameAPI has generic request method

--- my-app/src/helper_functions/helpers.js
+++ my-app/src/helper_functions/helpers.js
@@ -1,4 +1,5 @@
-class BandNameAPI {
+const BASE_URL = process.env.REACT_BASE_URL;
+class BandName {
   #name;
   #likes;
   constructor(name, likes) {
@@ -8,5 +9,30 @@ class BandNameAPI {
 }
 
 class BandNameAPI {
-  static request(endpoint, data) {}
+  static #token;
+  static async request(endpoint, data = {}, method = "get") {
+    const url = `${BASE_URL}/${endpoint}`;
+    console.debug("API Call:", url, endpoint, data, method);
+    const headers = { Authorization: `Bearer ${BandNameAPI.#token}` };
+    const params = method === "get" ? data : {};
+
+    try {
+      const responseData = await axios({ url, method, data, params, headers });
+      return responseData.data;
+    } catch (err) {
+      console.error("API Error:", err);
+      return err.response.data;
+    }
+  }
+  static async addBandName(bandName) {
+    try {
+      const data = await this.request("band/add", bandName, "post");
+    } catch (error) {}
+  }
+  static setToken(token) {
+    this.#token = token;
+  }
+  static getToken() {
+    return this.#token;
+  }
 }
