d765fb4fd330dcfdfe93886660180f440c624d51
Michael Wiltfong <wiltfong.michael1@gmail.com>
2023-04-15 16:31:11 -0400

    updating enviornment variables

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,10 +1,10 @@
 const Sequelize = require("sequelize");
 const dotenv = require("dotenv").config({ path: "../.env" });
-const user = process.env.DATABASE_USER;
-const host = process.env.DATABASE_HOST;
+const user = process.env.POSTGRES_USER;
+const host = process.env.POSTGRES_HOST;
 const database = "bandnameapi";
-const password = process.env.DATABASE_PW;
-const port = process.env.DATABASE_PORT;
+const password = process.env.POSTGRES_PASSWORD;
+const port = process.env.POSTGRES_PORT;
 
 const sequelize = new Sequelize(database, user, password, {
   host,
