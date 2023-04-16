a2756d5458eed99e1e733c5c735f6c06d5bda597
Michael Wiltfong <wiltfong.michael1@gmail.com>
2023-04-15 23:34:28 -0400

    implementing client.js file into yml file

---
+++ client.js
@@ -0,0 +1,18 @@
+const { Client } = require("pg");
+require("colors");
+const pgclient = new Client({
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: "postgres",
+  password: "postgres",
+  database: "postgres",
+});
+
+pgclient.connect();
+
+const database = "CREATE DATABASE bandnameapi;";
+
+pgclient.query(database, (err, res) => {
+  if (err) console.log(err.red);
+  else console.log("Database created successfully");
+});
